23fd750f49feec8b964c1a47ffc35372bff9a359
Deneziz <b00177540@mytudublin.ie>
2025-04-30 11:42:38 +0100

    Update project2.py
    here outlined the rest of potential options and created the button to add

--- project.py
+++ project.py
@@ -10,9 +10,8 @@ CREATE TABLE IF NOT EXISTS expenses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     item TEXT,
     amount REAL,
-    category TEXT,
+    type TEXT,
     date TEXT,
-    recurring INTEGER,
     payment_method TEXT,
     priority INTEGER
 )
@@ -31,6 +30,26 @@ item_entry.grid(row=0, column=1, pady=5)
 tk.Label(root, text="Amount:").grid(row=1, column=0, sticky="w", padx=10, pady=5)
 amount_entry = tk.Entry(root)
 amount_entry.grid(row=1, column=1, pady=5)
+
+tk.Label(root, text="Type:").grid(row=2, column=0, sticky="w", padx=10, pady=5)
+Types = ["Ocassional", "Bills", "Transport", "Entertainment", "Saving"]
+type_var = tk.StringVar()
+type_var.set(Types[0])
+type_menu = tk.OptionMenu(root, type_var, *Types)
+type_menu.grid(row=2, column=1, pady=5)
+
+
+tk.Label(root, text="Date (DD-MM-YYYY):").grid(row=3, column=0, sticky="w", padx=10, pady=5)
+date_entry = tk.Entry(root)
+date_entry.grid(row=3, column=1, pady=5)
+
+
+
+tk.Label(root, text="Payment Method:").grid(row=5, column=0, sticky="w", padx=10, pady=5)
+payment_var = tk.StringVar()
+payment_var.set("Cash")
+tk.Radiobutton(root, text="Cash", variable=payment_var, value="Cash").grid(row=5, column=1, sticky="w")
+tk.Radiobutton(root, text="Card", variable=payment_var, value="Card").grid(row=6, column=1, sticky="w")
 
 
 
